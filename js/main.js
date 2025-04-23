855238589236e55fab3ae63e5fa68de3f0e75a20
Your Name <you@example.com>
2025-04-22 23:24:53 -0600

    Functionality

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+let carouselDom = document.querySelector(".carousel");
+let listItemDom = document.querySelector(".carousel .list");
+let thumbnailDom = document.querySelector(".carousel .thumbnail")
+
+nextDom.onclick = function () {
+  showSlider("next");
+}
+  function showSlider(type){
+    let itemSlider = document.querySelectorAll(".carousel .list .item");
+    let itemThumbnail = document.querySelectorAll("carousel .thumbnail .item ")
+    
+    if(type === "next") {
+      listItemDom.appendChild(itemSlider[0]);
+    }
+  }
+
