ab3c7e2e63291d516f0d3f77c39c34ebf8e204f5
Your Name <you@example.com>
2025-04-27 22:41:23 -0600

    scroller

--- js/main.js
+++ js/main.js
@@ -50,3 +50,13 @@ function showSlider(type){
   }, timeAutoNext);
   }
 
+const scrollers = document.querySelectorAll(".scroller");
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation(); 
+}
+
+function addAnimation() {
+  scrollers.forEach(scroller => {
+    scroller.setAttribute('data-animated', true)
+  })
+}
